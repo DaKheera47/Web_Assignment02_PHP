406f1d913f05c5a0ebe487287be68a05bdfbfb5d
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2023-03-04 17:20:24 +0000

    Persistant dark mode

--- src/components/pageTop.component.php
+++ src/components/pageTop.component.php
@@ -9,9 +9,43 @@
     <title><?php echo $tab_title; ?></title>
 
     <script>
-        function toggleDarkMode() {
+        // enable dark mode
+        function enableDarkMode() {
             var element = document.documentElement;
-            element.classList.toggle("dark");
+            element.classList.add("dark");
+            // update dark mode in local storage
+            localStorage.setItem("darkMode", "enabled");
+        }
+
+        // disable dark mode
+        function disableDarkMode() {
+            var element = document.documentElement;
+            element.classList.remove("dark");
+            // update dark mode in local storage
+            localStorage.setItem("darkMode", null);
+        }
+
+        // check if dark mode is enabled
+        if (localStorage.getItem("darkMode") === "enabled") {
+            // enable dark mode
+            enableDarkMode();
+        } else {
+            // disable dark mode
+            disableDarkMode();
+        }
+
+        function handleTheme() {
+            // get from local storage
+            let darkMode = localStorage.getItem("darkMode");
+
+            // if dark mode is not enabled
+            if (darkMode !== "enabled") {
+                // enable dark mode
+                enableDarkMode();
+            } else {
+                // disable dark mode
+                disableDarkMode();
+            }
         }
 
         function handleNav() {
@@ -32,7 +66,7 @@
             </a>
 
             <div class="flex">
-                <button onclick="toggleDarkMode()" type="button" class="block md:hidden text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
+                <button onclick="handleTheme()" type="button" class="block md:hidden text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
                     <svg fill="currentColor" class="w-6 h-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
                     </svg>
@@ -71,7 +105,7 @@
                 </li>
 
                 <li class="w-full text-center">
-                    <button onclick="toggleDarkMode()" type="button" class="hidden md:block text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
+                    <button onclick="handleTheme()" type="button" class="hidden md:block text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
                         <svg fill="currentColor" class="w-6 h-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
                         </svg>
