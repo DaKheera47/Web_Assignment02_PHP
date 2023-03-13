52c28e54d038eae9e8f5def77841ccaf3c31dbb6
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2023-03-13 09:26:11 +0000

    Offers in accordion

--- src/components/pageTop.component.php
+++ src/components/pageTop.component.php
@@ -12,7 +12,6 @@ require_once "../components/connection.component.php";
     <link rel="stylesheet" href="../../output.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $tab_title; ?></title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
 
     <script>
         // enable dark mode
@@ -62,97 +61,6 @@ require_once "../components/connection.component.php";
 </head>
 
 <body class="dark:bg-gray-900 dark:text-white pt-16 md:pt-24">
-    <!-- <nav class="fixed top-0 left-0 w-full bg-uclan-red text-white border-gray-200 py-2.5 md:py-0 rounded px-[5vw]">
-        <div class="container flex flex-wrap md:flex-nowrap items-center justify-between mx-auto max-w-screen-xl">
-            <a href="./index.html" class="flex items-center">
-                <img src="../../public/images/uclanLogo.jpg" class="h-6 mr-3 sm:h-9 rounded-full" alt="UCLan Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap">
-                    UCLan Student Shop
-                </span>
-            </a>
-
-            <div class="flex">
-                <button onclick="handleTheme()" type="button" class="block md:hidden text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
-                    <svg fill="currentColor" class="w-6 h-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-
-                <button onclick="handleNav()" class="block md:hidden text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
-                    <svg fill="currentColor" class="w-6 h-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </div>
-
-            <ul id="nav-content" class="hidden w-full md:w-3/5 lg:w-2/5 md:flex flex-col p-4 my-4 text-white rounded-lg justify-center items-center md:flex-row md:space-x-8 md:my-0 md:text-sm md:font-medium md:border-0 divide-y-2 md:divide-y-0">
-                <li class="w-full text-center inline-flex items-center justify-center">
-                    <a href="./index.php" class="block py-5 md:py-2 rounded hover:text-gray-200 md:bg-transparent md:p-0" aria-current="page">
-                        Home
-                    </a>
-                </li>
-
-                <li class="w-full text-center inline-flex items-center justify-center">
-                    <a href="./products.php" class="block py-5 md:py-2 rounded hover:text-gray-200 md:bg-transparent md:p-0">
-                        Products
-                    </a>
-                </li>
-
-                <?php if (isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"] == true) { ?>
-                    <li class="w-full text-center inline-flex items-center justify-center">
-                        <a href="./cart.php" class="block py-5 md:py-2 rounded hover:text-gray-200 md:bg-transparent md:p-0">
-                            Cart
-                        </a>
-                    </li>
-                <?php } ?>
-
-                <li class="w-full text-center inline-flex items-center justify-center">
-                    <button onclick="handleTheme()" type="button" class="hidden md:block text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
-                        <svg fill="currentColor" class="w-6 h-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
-                        </svg>
-                    </button>
-                </li>
-
-                <?php if (isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"] == true) { ?>
-                    <li class="inline-flex w-full text-center items-center justify-center">
-                        <a href="profile.php" class="hidden md:block text-white focus:outline-none hover:text-uclan-red hover:bg-gray-100 rounded-lg text-sm p-2.5">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    </li>
-
-                    <form action="./logout.php" method="POST">
-                        <input type="hidden" name="action" value="logout">
-                        <li class="w-full text-center inline-flex items-center justify-center">
-                            <button type="submit" class="block py-5 md:py-2 rounded hover:text-gray-200 md:bg-transparent md:p-0 whitespace-nowrap">
-                                Log Out
-                            </button>
-                        </li>
-                    </form>
-
-                <?php } else { ?>
-                    <li class="w-full text-center inline-flex items-center justify-center">
-                        <a href="./login.php" class="block py-5 md:py-2 rounded hover:text-gray-200 md:bg-transparent md:p-0">
-                            Login
-                        </a>
-                    </li>
-
-                    <li class="w-full text-center inline-flex items-center justify-center group">
-                        <a href="./signup.php" class="hover:bg-white hover:text-uclan-red bg-uclan-red text-white focus:outline-none font-bold rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center whitespace-nowrap">
-                            Sign Up
-
-                            <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </li>
-                <?php } ?>
-            </ul>
-        </div>
-    </nav> -->
-
     <nav class="fixed top-0 left-0 w-full bg-uclan-red text-white border-gray-200 px-[5vw] py-2.5 rounded">
         <div class="container flex flex-wrap items-center justify-between mx-auto max-w-screen-xl">
             <a href="./index.php" class="flex items-center">
--- src/pages/index.php
+++ src/pages/index.php
@@ -44,23 +44,38 @@ require_once "../components/pageTop.component.php";
         Currently Running Offers
     </h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+    <!-- <div class="grid grid-cols-1 md:grid-cols-3 gap-4"> -->
+    <div class="grid grid-cols-1">
         <?php
         require_once("../components/connection.component.php");
 
         $q = "SELECT * FROM tbl_offers";
         $res = mysqli_query($conn, $q);
 
-        while ($row = mysqli_fetch_array($res)) {
-            echo '
-<div class="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg dark:bg-gray-800 bg-blue-50 dark:text-blue-400 dark:border-blue-800">
-    <h3 class="text-lg font-medium capitalize">' . $row["offer_title"] . '</h3>
-    <span class="my-2 text-sm dark:text-white">' . $row["offer_dec"] . '</span>
-</div>
-';
-        }
         ?>
-    </div>
-</div>
 
-<?php require_once "../components/pageBottom.component.php"; ?>
+        <div id="accordion-collapse" data-accordion="collapse">
+            <?php while ($row = mysqli_fetch_array($res)) { ?>
+                <!-- heading -->
+                <h2 id=<?php echo 'heading-' . $row["offer_id"] ?>>
+                    <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target=<?php echo '#body-' . $row["offer_id"] ?> aria-expanded="true" aria-controls=<?php echo 'body-' . $row["offer_id"] ?>>
+                        <span class="capitalize"><?php echo $row["offer_title"] ?></span>
+                        <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </h2>
+
+                <!-- content -->
+                <div id=<?php echo 'body-' . $row["offer_id"] ?> class="hidden" aria-labelledby=<?php echo 'heading-' . $row["offer_id"] ?>>
+                    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                        <p class="mb-2 text-gray-800 dark:text-gray-200">
+                            <?php echo $row["offer_dec"] ?>
+                        </p>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+    <?php require_once "../components/pageBottom.component.php"; ?>
