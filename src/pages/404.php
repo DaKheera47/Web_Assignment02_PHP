883d8517d83efb66bfecad016cdae089a55033aa
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2023-03-22 21:26:35 +0000

    404 page with .htaccess

---
+++ src/pages/404.php
@@ -0,0 +1,32 @@
+<?php
+$tab_title = "Profile Page - UCLan Student Shop";
+require_once "../components/pageTop.component.php";
+?>
+
+<div class="flex flex-col items-center text-center justify-center w-full h-[calc(100vh-100px)] space-y-4">
+    <!-- <div class="flex flex-col items-center justify-center w-full text-center lg:w-1/2 lg:px-2 xl:px-0"> -->
+        <div class="flex flex-wrap items-center justify-between md:flex-nowrap">
+            <p class="w-full font-mono text-3xl font-bold tracking-wider text-gray-700 md:w-fit dark:text-gray-300 md:text-7xl">404</p>
+            <div class="hidden w-px h-full mx-4 bg-black md:block dark:bg-white"></div>
+            <p class="w-full font-mono text-xl font-bold text-gray-700 md:w-fit md:text-4xl md:whitespace-nowrap dark:text-gray-300">This page could not be found.</p>
+        </div>
+
+        <p class="mt-8 mb-4 font-mono text-base text-gray-600 dark:text-gray-400">Orient Yourself</p>
+
+        <div class="inline-flex rounded-md shadow-sm" role="group">
+            <a href="./index.php" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                Home
+            </a>
+
+            <a href="./products.php" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                Products
+            </a>
+        <!-- </div> -->
+    </div>
+</div>
+
+</main>
+
+</body>
+
+</html>
