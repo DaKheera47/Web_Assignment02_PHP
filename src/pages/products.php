9e66737c101d779d4ef1d62dc1214ba7888c0ade
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2023-03-23 14:49:32 +0000

    Search results

--- src/pages/products.php
+++ src/pages/products.php
@@ -15,6 +15,16 @@ if (isset($_GET['sort']) && $_GET['sort'] != "All") {
     $stmt = $conn->prepare($sql);
 }
 
+// search_query
+if (isset($_GET['search_query']) && !empty($_GET['search_query'])) {
+    $search_query = htmlspecialchars($_GET['search_query']);
+    // prepare a query
+    $sql = "SELECT * FROM tbl_products WHERE product_title LIKE ? OR product_type LIKE ? ORDER BY product_type";
+    $stmt = $conn->prepare($sql);
+    $search_query = "%" . $search_query . "%";
+    $stmt->bind_param("ss", $search_query, $search_query);
+}
+
 $stmt->execute();
 $result = $stmt->get_result();
 
@@ -55,9 +65,18 @@ $_SESSION["error"] = "";
 
 if ($result->num_rows > 0) { ?>
 
-    <h1 class="mt-8 mb-4 heading">
-        You can view all our available products here
-    </h1>
+    <?php if (isset($_GET['search_query']) && !empty($_GET['search_query'])) {
+        $search_query = htmlspecialchars($_GET['search_query']); ?>
+        <h2 class='mb-4 text-uclan-yellow text-2xl capitalize'>Showing Search results for: <?php echo $search_query ?> </h2>
+    <?php } else if (isset($_GET['sort']) && !empty($_GET['sort'])) { ?>
+        <h2 class='mb-4 text-uclan-yellow text-2xl capitalize'>
+            Showing <?php echo $_GET['sort'] ?> Products
+        </h2>
+    <?php } else { ?>
+        <h2 class='mb-4 text-uclan-yellow text-2xl capitalize'>
+            Showing all products
+        </h2>
+    <?php } ?>
 
     <div class="my-4">
         <div class="grid flex-wrap items-center justify-center grid-cols-2 grid-rows-2 gap-2 rounded-md shadow-sm sm:inline-flex sm:gap-0 sm:flex-nowrap">
@@ -92,9 +111,22 @@ if ($result->num_rows > 0) { ?>
                     UCLan Hoodie
                 </button>
             </form>
-
         </div>
     </div>
+
+    <form class="my-4">
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+            </div>
+
+            <input type="search" name="search_query" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+    </form>
 
     <!-- <div class="grid w-full grid-cols-1 gap-4"> -->
     <div class="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
