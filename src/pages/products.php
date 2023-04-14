f309bfb5472fd319bf1ea71c3848417e14038296
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2023-04-14 19:29:04 +0100

    Fixing validator issues

--- src/pages/products.php
+++ src/pages/products.php
@@ -123,7 +123,7 @@ if ($result->num_rows > 0) { ?>
                 </svg>
             </div>
 
-            <input type="search" name="search_query" id="default-search" class="z-50 block w-full p-4 pl-10 text-sm text-gray-900 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+            <input type="search" name="search_query" id="default-search" class="z-50 block w-full p-4 pl-10 text-sm text-gray-900 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search our catalogue..." required>
             <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
         </div>
     </form>
