114b823d39feeaddbca7af1f69351d8ea76b2b94
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2023-04-14 18:12:47 +0100

    Favicon

--- src/components/pageTop.component.php
+++ src/components/pageTop.component.php
@@ -8,7 +8,7 @@ require_once "../components/connection.component.php";
 
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/svg+xml" href="/vite.svg">
+    <link rel="icon" type="image/png" href="/~ssarfaraz/public/images/favicon.png">
     <link rel="stylesheet" href="../../output.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $tab_title; ?></title>
